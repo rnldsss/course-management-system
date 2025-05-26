201b0565d525de8a3246a7b81b2317d6bbae1c65
tazkyamutia <tazkyamr34@example.com>
2025-05-26 23:00:23 +0700

    coba

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.coursemanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
+    requires transitive java.sql;
     requires transitive javafx.graphics;
 
     opens com.coursemanagementsystem to javafx.fxml;
